171dcc0cdd49b5cef672b388bd29f39eb77fa9fb
Panayot Kindalov <panayotkindalov@gmail.com>
2016-05-26 10:47:35 +0300

    A marketing company wants to keep record of its employees. Each record would have the following characteristics: first name, last name, age(0-100), gender(m or f), Personal ID number(e.g. 8306112507), Unique employee number(27560000…27569999)

--- EmployeeData/EmployeeData.cs
+++ EmployeeData/EmployeeData.cs
@@ -1,4 +1,4 @@
-ï»¿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,16 @@ namespace EmployeeData
     {
         static void Main(string[] args)
         {
+		
+			//A marketing company wants to keep record of its employees. Each record would have the following characteristics:
+			//•	First name
+			//•	Last name
+			//•	Age (0...100)
+			//•	Gender (m or f)
+			//•	Personal ID number (e.g. 8306112507)
+			//•	Unique employee number (27560000…27569999)
+
+		
             string firstName = Console.ReadLine();
             string lastName = Console.ReadLine();
             byte age = byte.Parse(Console.ReadLine());
